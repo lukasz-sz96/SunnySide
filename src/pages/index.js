efa4ec564327de76b48db8663b93f671692258ff
Kalmar <froozonezks@gmail.com>
2020-08-23 20:24:19 +0200

    Initial weather fetch logic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,36 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
 
 import SEO from "../components/seo"
+import { getWeather } from "../utils/getWeather"
 
-const IndexPage = () => (
-  <div>
-    <SEO title="Home" />
-  </div>
-)
+const IndexPage = () => {
+  const [weather, setWeather] = useState()
+
+  const handleInput = e => {
+    if (e.keyCode == 13) {
+      getWeather(e.target.value).then(data => {
+        if (data) {
+          setWeather(data)
+        }
+      })
+    }
+  }
+
+  return (
+    <div>
+      <SEO title="Home" />
+      <input type="text" placeholder="Input city name" onKeyDown={handleInput} />
+      {weather ? (
+        <div>
+          <h2>{weather.name}</h2>
+          <h2>Temperature: {weather.main.temp}</h2>
+        </div>
+      ) : (
+        <h2>No data</h2>
+      )}
+    </div>
+  )
+}
 
 export default IndexPage
