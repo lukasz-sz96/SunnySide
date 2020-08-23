8f2c0ebf6bcdd439e2bbac700e51dea4e580e6df
Kalmar <froozonezks@gmail.com>
2020-08-23 17:29:51 +0200

    Cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `SunnySide`,
+    description: `Weather report web app.`,
+    author: `Łukasz Szczęsny`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
