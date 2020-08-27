99d22b2e159cb360a7d4debd34613217a7c7b606
Kalmar <froozonezks@gmail.com>
2020-08-28 00:28:15 +0200

    Change ForecastBox styling

--- src/components/ForecastBox.jsx
+++ src/components/ForecastBox.jsx
@@ -4,19 +4,20 @@ import React, { useState } from "react"
 
 const StyledDiv = styled(motion.div)`
   display: inline-block;
-  bottom: 0;
-  padding: 0.5em;
+  padding: 0.45em;
   margin: 0.5em;
   background: white;
-  border: 5px 5px 5px black;
+  border: 1px solid black;
   border-radius: 3px;
-  width: 18.1%;
+  width: auto;
+  overflow: hidden;
 `
 
 const ForecastBox = props => {
-  const [variant, setVariant] = useState("show")
-  const container = {
-    hidden: { y: "100px", opacity: 0 },
+  const {variant} = props
+
+  const containerAnim = {
+    hide: { y: "100px", opacity: 0 },
     show: {
       y: "0px",
       opacity: 1,
@@ -25,17 +26,27 @@ const ForecastBox = props => {
       },
     },
   }
-  const item = {
-    show: { opacity: 1, x: 0, transition: {
-        delay: 0.2
-    } },
-    hidden: { opacity: 0, x: -50 }
+
+  const itemAnim = {
+    show: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: 0.2,
+        duration: 0.4,
+      },
+    },
+    hide: { opacity: 0, x: -50 },
   }
+
   return (
-    <StyledDiv variants={container} initial="hidden" animate={variant}>
-      <motion.div variants={item} initial="hidden" animate={variant}>{props.children}</motion.div>
+    <StyledDiv variants={containerAnim} initial="hide" animate={variant}>
+      <motion.div variants={itemAnim} initial="hide" animate={variant}>
+        {props.children}
+      </motion.div>
     </StyledDiv>
   )
 }
 
 export default ForecastBox
+
