d8e78a98743255db64e8f74b1fa95b6237f6ef79
Kalmar <froozonezks@gmail.com>
2020-09-06 01:00:07 +0200

    Code structure

--- src/components/Sunny.jsx
+++ src/components/Sunny.jsx
@@ -1,12 +1,12 @@
-import React, { useRef, useState, useEffect } from "react"
+import React, { useRef, useEffect } from "react"
 import Sun from "../images/sun.svg"
 import gsap from "gsap"
 
 const style = {
   position: "absolute",
   zIndex: -5,
-  top: '-20vh',
-  left: '-20vh',
+  top: "-20vh",
+  left: "-20vh",
 }
 
 const Sunny = () => {
@@ -15,13 +15,23 @@ const Sunny = () => {
   useEffect(() => {
     const [elements] = wrapper.current.children
     console.log(elements)
-      const sunrays = elements.getElementById('sunrays')
-      const sun = elements.getElementById('sun')
+    const sunrays = elements.getElementById("sunrays")
+    const sun = elements.getElementById("sun")
 
-      gsap.set([sun, sunrays], {autoAlpha: 0,  zIndex: -5, transformOrigin:"50% 50%" })
-      const tl = gsap.timeline({});
-      tl.to([sun, sunrays], {duration: 1, autoAlpha: 1});
-      tl.to([sun, sunrays], {duration: 10, autoAlpha: 1, rotation: 360, repeat: -1, yoyo:true});
+    gsap.set([sun, sunrays], {
+      autoAlpha: 0,
+      zIndex: -5,
+      transformOrigin: "50% 50%",
+    })
+    const tl = gsap.timeline({})
+    tl.to([sun, sunrays], { duration: 1, autoAlpha: 1 })
+    tl.to([sun, sunrays], {
+      duration: 50,
+      autoAlpha: 1,
+      rotation: 360,
+      repeat: -1,
+      yoyo: true,
+    })
   })
 
   return (
